c4f462cb508a6caf899b1e7ac0f32c5e929690b0
root <root@localhost.localdomain>
2018-12-15 12:36:44 +0300

    Рефакторинг.

--- md1-queue/Program.cs
+++ md1-queue/Program.cs
@@ -9,47 +9,7 @@ namespace md1_queue
 {
     class Client
     {
-        /*
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string FullName
-        {
-            get
-            {
-                return FirstName + " " + LastName;
-            }
-        }
-        */
         public string FullName { get; set; }
-
-        static string GetFirstName()
-        {
-            List<string> names = new List<string>();
-            names.Add("Вася");
-            names.Add("Женя");
-            names.Add("Саня");
-            names.Add("Коля");
-            Random rand = new Random();
-            return names[(rand.Next() % names.Count())];
-        }
-
-        static string GetLastName()
-        {
-            List<string> names = new List<string>();
-            names.Add("Иванов");
-            names.Add("Петров");
-            names.Add("Сидоров");
-            names.Add("Кароль");
-            Random rand = new Random();
-            return names[(rand.Next() % names.Count())];
-        }
-
-        public Client()
-        {
-            //this.FirstName = GetFirstName();
-            //this.LastName = GetLastName();
-        }
     }
 
     class md1
@@ -57,9 +17,9 @@ namespace md1_queue
         private Queue<Client> queue { get; set; }
         public double lambda { get; set; }
         public double tn { get; set; }
-        public int n { get; set; }
-        public int m { get; set; }
-        public int w { get; set; }
+        public double n { get; set; }
+        public double m { get; set; }
+        public double w { get; set; }
         static Semaphore sem { get; set; }
         private int Count { get; set; }
 
@@ -68,13 +28,13 @@ namespace md1_queue
             //Count++;
             sem.WaitOne();
             Console.WriteLine("{0} Start {1} {2}", DateTime.Now, Thread.CurrentThread.Name, Count);
-            Thread.Sleep(w);
+            Thread.Sleep((int)w);
             sem.Release();
             Count--;
             Console.WriteLine("{0} Stop {1} {2}", DateTime.Now, Thread.CurrentThread.Name, Count);
         }
 
-        public md1(double lambda = 1, double tn = 10, int n = 3, int m = 6, int w = 5000)
+        public md1(double lambda = 1, double tn = 10, double n = 3, double m = 6, double w = 5000)
         {
             this.lambda = lambda;
             this.tn = tn;
@@ -82,8 +42,8 @@ namespace md1_queue
             this.m = m;
             this.w = w;
             Count = 0;
-            queue = new Queue<Client>(m);
-            sem = new Semaphore(n, n);
+            queue = new Queue<Client>((int)m);
+            sem = new Semaphore((int)n, (int)n);
         }
 
         public void addClient(Client client)
@@ -134,29 +94,33 @@ namespace md1_queue
 
     class Program
     {
+        static double fact(double n)
+        {
+            if (n == 1)
+                return n;
+            if (n == 0)
+                return 1;
+            return n * fact(n - 1);
+        }
+
         static void Main(string[] args)
         {
-            md1 md = new md1();
-            md.GenPoisson();
+            Console.Write("Интенсивность потока Lambda(1) = ");
+            double lambda = Double.Parse(Console.ReadLine());
 
-            //Console.Write("\nPress any key to continue... ");
-            Console.ReadKey();
+            Console.Write("Время моделирования Tn(10) = ");
+            double tn = Double.Parse(Console.ReadLine());
 
-            /*
-            Console.Write("Число каналов n = ");
-            double n = Double.Parse(Console.ReadLine());
+            Console.Write("Число каналов n(3) = ");
+            double n = int.Parse(Console.ReadLine());
 
-            Console.Write("Длина очереди m = ");
-            double m = Double.Parse(Console.ReadLine());
+            Console.Write("Длина очереди m(6) = ");
+            double m = int.Parse(Console.ReadLine());
 
-            Console.Write("Кол-во заявок k = ");
-            double k = Double.Parse(Console.ReadLine());
+            Console.Write("Время на заявку t(5) = ");
+            double t = int.Parse(Console.ReadLine());
 
-            Console.Write("Время на одну заявку t(min) = ");
-            double t = Double.Parse(Console.ReadLine());
-
-            double lambda = k / (60 * 24);
-            Console.WriteLine("labda = {0}", lambda);
+            Console.WriteLine();
 
             double mu = 1 / t;
             Console.WriteLine("mu = {0}", mu);
@@ -165,14 +129,13 @@ namespace md1_queue
             Console.WriteLine("psi = {0}", psi);
 
             double sum = 0.0;
-
             for (int i = 0; i <= n; i++)
             {
                 sum += (Math.Pow(n, i) / fact(i)) * Math.Pow(psi, i);
             }
 
             double p0 = Math.Pow(sum + ((n / fact(n)) * ((Math.Pow(psi, n + 1) * (1 - Math.Pow(psi, m))) / (1 - psi))), -1);
-            Console.WriteLine("p0 = {0}", p0 );
+            Console.WriteLine("p0 = {0}", p0);
 
             double pr = (Math.Pow(n, n) / fact(n)) * Math.Pow(psi, n + m) * p0;
             Console.WriteLine("pr = {0}", pr);
@@ -200,20 +163,14 @@ namespace md1_queue
 
             double t0 = ts + tl;
             Console.WriteLine("T = {0}", t0);
+            Console.WriteLine();
 
-            //double p0 = Math.Pow(1 + (Math.Pow(n, 1) / fact(1)) + (Math.Pow(n, 2) / fact(2)) + (Math.Pow(n, 3) / fact(3)) + (Math.Pow(n, 4) / fact(4)) * 4, -1);
-            //
+            md1 md = new md1(lambda, tn, n, m, t * 1000);
+            md.GenPoisson();
+            
 
-            //Console.WriteLine("p0 = {0}", p0);
-
-            //for (int i = 1; i < 500; i++)
-            //{
-            //    Reader reader = new Reader(i);
-            //}
-
-            Console.Write("\nPress any key to continue... ");
+            //Console.Write("\nPress any key to continue... ");
             Console.ReadKey();
-            */
         }
     }
 }
